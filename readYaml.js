df70128a8f56352cd243a7c3c960ef18b7cb5de1
Siva Swami <sivaarulveli@gmail.com>
2022-02-27 12:59:49 +0000

    test yaml

--- readYaml.js
+++ readYaml.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
         $(document).ready(function () {
-            $.get('_data/summary.yaml')
+            $.get('https://github.com/sivaswamigal/sivaadhistanam/blob/main/_data/summary.yaml')
             .done(function (data) {
               console.log('File load complete');
               console.log(jsyaml.load(data));
