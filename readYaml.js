282e7aeb512fd39d9e597d5f1ffc8bee7c11b77e
Siva Swami <sivaarulveli@gmail.com>
2022-02-27 13:55:45 +0000

    test yaml

--- readYaml.js
+++ readYaml.js
@@ -4,12 +4,8 @@
             $.get('_data/summary.yaml')
             .done(function (data) {
               console.log('File load complete');
-              var data=jsyaml.load(data);
-              console.log('Test 1');
-              console.log(data[0].valueOf('Amount'));
-              console.log('test with third bracket');
+                data=jsyaml.load(data);
               console.log(data[0]['Amount']);
-              console.log(data[0][1]);
               var jsonString = JSON.stringify(data);
               console.log(jsonString);
               console.log($.parseJSON(jsonString));
