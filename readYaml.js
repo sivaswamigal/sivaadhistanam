ba78362d0bab85804c663913672b55e60f8bc9ad
Siva Swami <sivaarulveli@gmail.com>
2022-02-27 14:03:33 +0000

    test yaml

--- readYaml.js
+++ readYaml.js
@@ -1,14 +1,6 @@
 (function () {
     "use strict";
         $(document).ready(function () {
-            $.get('_data/summary.yaml')
-            .done(function (data) {
-              console.log('File load complete');
-                yamlData=jsyaml.load(data);
-              console.log(yamlData[0]['Amount']);
-              var jsonString = JSON.stringify(yamlData);
-              console.log(jsonString);
-              console.log($.parseJSON(jsonString));
-          });
+            
         });
     }());
